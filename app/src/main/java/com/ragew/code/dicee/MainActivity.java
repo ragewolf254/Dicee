6b27ccc8a465beae737790b77092b04b6f580171
ragewolf254 <ragewolf254@yahoo.com>
2018-01-09 11:27:50 +0800

    Added button functionalities for random die appearances

--- app/src/main/java/com/ragew/code/dicee/MainActivity.java
+++ app/src/main/java/com/ragew/code/dicee/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,14 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
         Button rollButton = findViewById(R.id.rollButton);
 
-        ImageView leftDie = findViewById(R.id.leftDie);
+        final ImageView leftDie = findViewById(R.id.leftDie);
 
-        ImageView rightDie = findViewById(R.id.rightDie);
+        final ImageView rightDie = findViewById(R.id.rightDie);
+
+        final int[] myDiceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
 
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Dicee", "The button has been pressed!");
+                final Random randomNumber = new Random();
+                int number = randomNumber.nextInt(6);
+                leftDie.setImageResource(myDiceArray[number]);
+                number = randomNumber.nextInt(6);
+                rightDie.setImageResource(myDiceArray[number]);
             }
         });
     }
